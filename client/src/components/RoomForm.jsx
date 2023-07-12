81ee3331bb7ca94fb5ced5021f673a1f8f45c3ed
Eric-Zanette <ericzanette@gmail.com>
2023-07-12 11:34:39 -0400

    added room routing

--- client/src/components/RoomForm.jsx
+++ client/src/components/RoomForm.jsx
@@ -2,28 +2,61 @@ import React from "react";
 import { useState } from "react";
 
 const RoomForm = () => {
-  const [input, setInput] = useState();
+  const [input, setInput] = useState({
+    join: "",
+    create: "",
+  });
+
+  const onChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const boardName = input[e.target.name];
+    fetch("http://localhost:5000/match", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ [e.target.name]: boardName }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
 
   return (
-    <>
+    <div className="formsContainer">
       <h1>Play a Game!</h1>
-      <form>
-        <div className="formLine">
-          <div className="line">
-            <h2>Join Existing Game Room</h2>
-            <input placeholder="Game Name" />
-          </div>
-          <button>Join</button>
+      <form name="join" onSubmit={(e) => onSubmit(e)}>
+        <div className="line">
+          <h2>Join Existing Game Room</h2>
+          <input
+            placeholder="Game Name"
+            name="join"
+            value={input.join}
+            onChange={(e) => onChange(e)}
+          />
         </div>
-        <div className="formLine">
-          <div className="line">
-            <h2>Create a New Game</h2>
-            <input placeholder="Game Name" />
-          </div>
-          <button>Create</button>
-        </div>
+        <button name="join">Join</button>
       </form>
-    </>
+      <form name="create" onSubmit={(e) => onSubmit(e)}>
+        <div className="line">
+          <h2>Create a New Game</h2>
+          <input
+            placeholder="Game Name"
+            name="create"
+            value={input.create}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <button name="create">Create</button>
+      </form>
+    </div>
   );
 };
 
--- server.py
+++ server.py
@@ -1,29 +1,73 @@
-from flask import Flask
-from flask_socketio import SocketIO, send
+from flask import Flask, request, session
+from flask_socketio import SocketIO, send, join_room, leave_room
 import Chess.ChessObjects as chess
 from flask_cors import CORS
 import json
 
-
-board = chess.Gameboard()
-board.reset()
-state = board.slice_all()
-jsonState = json.dumps(state)
+boards = {}
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "fsdfsdfsdf"
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+""" Initializes board  """
 
 
 @app.route("/board", methods=["POST", "GET"])
 def home():
-    return {"board": state, "player": board.player}
+    room = session.get("room")
+    if room is None or room not in boards:
+        return False
+    return {"board": boards[room].slice_all(), "player": boards[room].player}
+
+
+""" Creates or joins gameroom """
+
+
+@app.route("/match", methods=["GET", "POST"])
+def match():
+    create = request.get_json().get("create")
+    join = request.get_json().get("join")
+
+    if create is not None:
+        if create in boards.keys():
+            return {"error": "game already exists"}
+        board = chess.Gameboard()
+        board.reset()
+        boards[create] = board
+        session["room"] = create
+    elif join is not None:
+        if join not in boards.keys():
+            return {"error": "Games does not exist"}
+        session["room"] = join
+    else:
+        return {"error": "must enter game room name"}
+
+    print(boards)
+    return {session["room"]: boards[session["room"]].slice_all()}
+
+
+""" Joins gameroom """
+
+
+@app.route("/room")
+def room():
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in boards.keys():
+        return False
+
+    return True
+
+
+""" Recieves move and sends back board state """
 
 
 @socketio.on("message")
 def message(move1, move2):
-    global state
+    room = session["room"]
+    board = boards[room]
     board.make_a_move(move1, move2)
     state = board.slice_all()
 
@@ -33,7 +77,15 @@ def message(move1, move2):
             inCheck = "You're in Checkmate!"
     else:
         inCheck = "Not in Check"
-    send({"board": state, "player": board.player, "inCheck": inCheck})
+    send(
+        {
+            "gameroom": session["room"],
+            "board": state,
+            "player": board.player,
+            "inCheck": inCheck,
+        },
+        to=room,
+    )
 
 
 if __name__ == "__main__":
