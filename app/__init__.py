4823c316196eee41816ebdf51065b5e2c8e83038
Eric-Zanette <ericzanette@gmail.com>
2023-07-19 13:22:49 -0400

    serving react build through flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_file
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -6,7 +6,7 @@ from flask_socketio import SocketIO
 from flask_cors import CORS
 
 
-app = Flask(__name__, static_folder="client/build", static_url_path="/")
+app = Flask(__name__, static_folder="../client/build", static_url_path="/")
 app.config.from_object(Config)
 CORS(app, supports_credentials=True)
 socketio = SocketIO(app, cors_allowed_origins="*")
@@ -18,12 +18,7 @@ from app.routes import boardRoutes, userRoutes, gameRoutes
 
 @app.route("/")
 def index():
-    return app.send_static_file("index")
-
-
-@app.route("/")
-def server():
-    return send_from_directory(app.static_folder, "index.html")
+    return app.send_static_file("index.html")
 
 
 if __name__ == "__main__":
